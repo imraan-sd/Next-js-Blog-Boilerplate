ecda4fe1dc6be8cf955bb84a8cbdaca94d6c53fc
Imraan Abbaskoor <imraan.abbasakoor@sdworx.com>
2025-02-04 17:57:04 +0400

    add basepath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const baseUrl = '';
+const baseUrl = '/sdblog';
 
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
